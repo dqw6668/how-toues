e850d73d166fe3fcfbd39efc0e9665674b16b144
dqw6668 <yjdqw@126.com>
2018-04-23 22:59:36 +0800

    problem20

---
+++ Elementary/Easy Unpack.py
@@ -0,0 +1,6 @@
+def easy_unpack(elements):
+    """
+        returns a tuple with 3 elements - first, third and second to the last
+    """
+    # your code here
+    return (elements[0], elements[2], elements[-2])  
